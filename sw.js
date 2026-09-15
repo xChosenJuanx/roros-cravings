566926402f9b242cce0765ebd48db78d4b5b1809
xChosenJuanx <112453576+xChosenJuanx@users.noreply.github.com>
2026-09-15 10:58:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,54 @@
-const CACHE='roros-v1';const ASSETS=['./','index.html','style.css','app.js','manifest.json','assets/logo.png','assets/hungarian.png','assets/samgyup.png','assets/soy-garlic.png','assets/shawarma.png','assets/donkatsu.png','assets/bibimbap.png','assets/gochujang.png','assets/chick-fries.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'roros-v2';
+
+const ASSETS = [
+  './',
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json',
+  'assets/logo.png',
+  'assets/hungarian.png',
+  'assets/samgyup.png',
+  'assets/soy-garlic.png',
+  'assets/shawarma.png',
+  'assets/donkatsu.png',
+  'assets/bibimbap.png',
+  'assets/gochujang.png',
+  'assets/chick-fries.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
