8a2b705d28e188fd81fe0e0cbcf9abf598b72e91
xChosenJuanx <112453576+xChosenJuanx@users.noreply.github.com>
2026-09-18 07:43:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'roros-v4';
+const CACHE = 'roros-v5';
 
 const ASSETS = [
   './',
@@ -27,13 +27,15 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
       )
-    ).then(() => self.clients.claim())
+      .then(() => self.clients.claim())
   );
 });
 
